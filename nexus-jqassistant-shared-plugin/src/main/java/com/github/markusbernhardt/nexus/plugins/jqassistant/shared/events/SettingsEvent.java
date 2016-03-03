4f76c647fabfd1dae9c3d056e7ea373e8c466dde
Markus Bernhardt <markus.bernhardt@me.com>
2016-03-03 23:57:47 +0100

    Moved GPL statement before package in java files

--- nexus-jqassistant-shared-plugin/src/main/java/com/github/markusbernhardt/nexus/plugins/jqassistant/shared/events/SettingsEvent.java
+++ nexus-jqassistant-shared-plugin/src/main/java/com/github/markusbernhardt/nexus/plugins/jqassistant/shared/events/SettingsEvent.java
@@ -1,6 +1,21 @@
-package com.github.markusbernhardt.nexus.plugins.jqassistant.shared.events;
 /*
  * Copyright (C) 2015 Markus Bernhardt
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+package com.github.markusbernhardt.nexus.plugins.jqassistant.shared.events;
+/*
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,11 +38,10 @@ import com.github.markusbernhardt.nexus.plugins.jqassistant.shared.model.Setting
 public class SettingsEvent extends AbstractEvent<Object> {
 
 	protected final SettingsXO settingsNew;
-
 	protected final SettingsXO settingsOld;
 
-	public SettingsEvent(Object sender, SettingsXO settingsNew, SettingsXO settingsOld) {
-		super(sender);
+	public SettingsEvent(Object component, SettingsXO settingsNew, SettingsXO settingsOld) {
+		super(component);
 		this.settingsNew = settingsNew;
 		this.settingsOld = settingsOld;
 	}
